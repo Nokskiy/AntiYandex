3601f6891fd99843cbeae3f70891909d865a841f
Nokskiy <145178078+mishaKan@users.noreply.github.com>
2025-02-23 19:53:13 +0500

    fail

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import os,psutil,getpass
+import getpass,shutil,os
+from pyqadmin import admin
 
 def get_user():
     return getpass.getuser()
-
-def locate_yandex():
-    return "C:/Users/" + get_user() + "/AppData/Local/Yandex/YandexBrowser/Application"
-print(locate_yandex())
+def get_disk_plus_user():
+    return "C:/Users/" + get_user()
+def locate_trash():
+    yandex = get_disk_plus_user() + "/AppData/Local/Yandex/YandexBrowser/Application"
+    yandex_roaming = get_disk_plus_user() + "/AppData/Roaming/Yandex"
+    yandex_local = get_disk_plus_user() + "/AppData/Local/Yandex"
+    yandex_programm_files = "C:/Program Files (x86)/Yandex"
+    trash = [yandex,yandex_roaming,yandex_local,yandex_programm_files]
+    return trash
+@admin
+def remove():
+    shutil.rmtree(r"C:/Program Files (x86)/Yandex")
+remove()
