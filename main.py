71d9cdb364c253daf1b7956f3e3a6b56acf5e792
Nokskiy <145178078+mishaKan@users.noreply.github.com>
2025-02-23 23:05:49 +0500

    linkers deleater work

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import getpass
 
-# import shutil
+import shutil
 import os
 from glob import glob
+from pyqadmin import admin
+import time
+from win32com.client import Dispatch
 
 username = getpass.getuser()
 home = os.path.join("C:", "Users", username)
 desktop = os.path.join(os.path.expanduser('~'), 'Desktop')
+
+
+def get_shortcut_target(shortcut_path):
+    shell = Dispatch("WScript.Shell")
+    shortcut = shell.CreateShortCut(shortcut_path)
+    return shortcut.Targetpath
 
 
 def locate_user_trash():
@@ -30,22 +39,19 @@ def locate_system_trash():
     )
 
 
+
 def locate_desktop_link():
-    return [
-        str(
-            glob(os.path.join(r"C:\Users\tsfka\OneDrive\Рабочий стол", '*.lnk'))
-        )
-    ]
-
-
+    return glob(os.path.join(R"C:\Users\tsfka\OneDrive\Рабочий стол", '*.lnk'))
+@admin
 def remove():
-    print(desktop)
+    os.remove(os.path.join(R"C:\Users",username, R"AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Yandex.lnk"))
     for link in locate_desktop_link():
-        print(link)
-    # for folder in locate_system_trash():
-    #    shutil.rmtree(folder)
-    # for folder in locate_user_trash():
-    #    shutil.rmtree(folder)
+        if "Yandex" in get_shortcut_target(link):
+            os.remove(link)
+    for folder in locate_system_trash():
+        shutil.rmtree(folder)
+    for folder in locate_user_trash():
+        shutil.rmtree(folder)
 
 
 remove()
